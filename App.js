e9473e70f749d04b458ab6b210d34870ba831091
Ruen Offerman <ruen_offerman@hotmail.com>
2018-08-23 10:47:11 +0200

    Implemented lottie for iOS (working)

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <LottieView
+          source={require('./star.json')}
+          autoPlay
+          loop
+        />
       </View>
     );
   }
